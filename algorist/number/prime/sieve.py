4e9ab000509f0a3625f3fbc05d61485293e513c6
munagekar <avm.abhishek@gmail.com>
2019-07-31 20:47:23 +0900

    Add Segmented Sieve

--- algorist/number/prime/sieve.py
+++ algorist/number/prime/sieve.py
@@ -1,9 +1,11 @@
+import bisect
+from math import sqrt, floor
 from typing import List
 
 
 # Reference : https://www.geeksforgeeks.org/sieve-of-eratosthenes/
 # Time Complexity : O(n)log(log(n))
-def eratosthenes(high: int) -> List[bool]:
+def sieve(high: int) -> List[int]:
     prime = [True] * (high + 1)
     prime[0] = False
     prime[1] = False
@@ -13,12 +15,12 @@ def eratosthenes(high: int) -> List[bool]:
             for j in range(i * i, high + 1, i):
                 prime[j] = False
 
-    return prime
+    return [idx for idx, is_prime in enumerate(prime) if is_prime]
 
 
 # Reference: https://www.hackerrank.com/topics/sieve-of-eratosthenes-linear-time
 # Time Complexity : O(n)
-def linear_eratosthenes(high: int) -> List[int]:
+def lsieve(high: int) -> List[int]:
     """
     A modification over eratosthenes which consumes more memory(~32x) but runs linearly
 
@@ -47,4 +49,50 @@ def linear_eratosthenes(high: int) -> List[int]:
             lp[target] = p
 
     return primes
+
+
+# Reference : https://www.geeksforgeeks.org/segmented-sieve/
+# Time Complexity : 0(n)log(log n)
+def seg_sieve(high: int, low: int = 0) -> List[int]:
+    """
+    Memory Optimized Sieve to quickly find primes in a range
+    Args:
+        high: non negative integer
+        low: non negative integer
+
+    Returns:
+        A list of prime numbers in the range
+    """
+
+    if low > high:
+        raise ValueError(f'Low={low} must be greater than high={high}')
+    # The return list
+    primes_in_range = []
+
+    prime_limit = floor(sqrt(high))
+    prime_list = sieve(prime_limit)
+
+    if low < prime_limit:
+        limit = bisect.bisect_left(prime_list, low)
+        primes_in_range.extend(prime_list[limit:])
+
+    # Get Segments
+    seg_low = max(prime_limit + 1, low)
+    seg_high = min(seg_low + prime_limit - 1, high)
+    while seg_low <= high:
+        prime_flag = [True] * (seg_high - seg_low + 1)
+        for p in prime_list:
+            start = seg_low // p * p
+            if start < seg_low:
+                start += p
+            for i in range(start, seg_high + 1, p):
+                prime_flag[i - seg_low] = False
+        primes_in_range.extend([idx + seg_low for idx, is_prime in enumerate(prime_flag) if is_prime])
+
+        seg_low = seg_high + 1
+        seg_high = min(seg_low + prime_limit - 1, high)
+
+    return primes_in_range
+
+
 
--- tests/number/prime/test_sieve.py
+++ tests/number/prime/test_sieve.py
@@ -1,29 +1,38 @@
-from algorist.number.prime.sieve import eratosthenes
-from algorist.number.prime.sieve import linear_eratosthenes
+import bisect
+
+from algorist.number.prime.sieve import lsieve
+from algorist.number.prime.sieve import seg_sieve
+from algorist.number.prime.sieve import sieve
 
 
-def test_erathosthenes():
-    primes = eratosthenes(23)
-    expected_primes = set([2, 3, 5, 7, 11, 13, 17, 19, 23])
-    for i in range(22):
-        if i in expected_primes:
-            assert (primes[i])
-        else:
-            assert (not primes[i])
+def test_sieve():
+    primes = sieve(23)
+    expected_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+    assert primes == expected_primes
 
 
-def test_linear_eratosthenes():
-    got = linear_eratosthenes(23)
+def test_lsieve():
+    got = lsieve(23)
     expected = [2, 3, 5, 7, 11, 13, 17, 19, 23]
     assert (got == expected)
 
 
 def test_linear_eratosthenes2():
-    expected = eratosthenes(1003)
-    got = linear_eratosthenes(1003)
-    print(got)
-    for idx, is_prime in enumerate(expected):
-        if idx in got:
-            assert is_prime
-        else:
-            assert (not is_prime)
+    expected = sieve(1003)
+    got = lsieve(1003)
+    assert expected == got
+
+
+def test_seg_sieve():
+    assert sieve(23) == seg_sieve(23)
+    assert sieve(32) == seg_sieve(32)
+    assert sieve(1000) == seg_sieve(1000)
+    s2000 = sieve(2000)
+    expected = s2000[bisect.bisect_left(s2000, 37):]
+    got = seg_sieve(2000, 37)
+    assert expected == got
+
+    assert seg_sieve(7, 6) == [7]
+    assert seg_sieve(7, 7) == [7]
+    assert seg_sieve(29, 27) == [29]
+    assert seg_sieve(30, 15) == [17, 19, 23, 29]
