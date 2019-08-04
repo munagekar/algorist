5c46ec91552dcf7c37d59b68f9a4be0ec1bf5422
munagekar <avm.abhishek@gmail.com>
2019-08-04 23:25:20 +0900

    Add Fenwick Low Memory

--- algorist/datastructure/tree/fenwick.py
+++ algorist/datastructure/tree/fenwick.py
@@ -60,4 +60,55 @@ class Fenwick:
         return self.cum_sum(high) - self.cum_sum(low - 1)
 
 
-# TODO: Add a Low memory version
+# 2x lesser space
+"""
+Read Complexity : 0(log(n))
+update : log(n)
+cum_sum : log(n)
+scale : 0(n)
+Memory: 2 * n
+"""
+
+
+class FenwickLowMemory:
+    def __init__(self, max_idx: int):
+        self._tree = [max_idx] + [0] * (max_idx)
+
+    def cum_sum(self, idx: int) -> float:
+        idx += 1
+        sum_ = 0
+        while idx > 0:
+            sum_ += self._tree[idx]
+            idx -= idx & -idx
+
+        return sum_
+
+    def ranged_cum_sum(self, low: int, high: int) -> float:
+        return self.cum_sum(high) - self.cum_sum(low - 1)
+
+    @classmethod
+    def from_array(cls, arr: List[float]):
+        f = cls(len(arr))
+        for idx, val in enumerate(arr):
+            f.update(idx, val)
+        return f
+
+    def update(self, idx: int, val: float):
+        idx += 1
+        while idx <= self._tree[0]:
+            self._tree[idx] += val
+            idx += idx & -idx
+
+    def scale(self, c: float):
+        self._tree = [i * c for i in self._tree]
+
+    def read(self, idx: int) -> float:
+        # Use bit ops to find common point
+        idx += 1
+        y = idx - 1
+        sum_ = self._tree[idx]
+        idx -= idx & -idx
+        while idx != y:
+            sum_ -= self._tree[y]
+            y -= y & -y
+        return sum_
--- tests/datastructure/tree/test_fenwick.py
+++ tests/datastructure/tree/test_fenwick.py
@@ -1,4 +1,4 @@
-from algorist.datastructure.tree.fenwick import Fenwick
+from algorist.datastructure.tree.fenwick import Fenwick, FenwickLowMemory
 import random
 import operator
 import itertools
@@ -21,4 +21,22 @@ def test_fenwick():
     assert f.read(3) == 10
     assert f.ranged_cum_sum(0, 3) == 16
     assert f._freq[4] == 10
+    assert f._tree[4] == 16
+
+
+def test_fenwick_low_mem():
+    array = [1, 2, 3, 4]
+    f = FenwickLowMemory.from_array(array)
+    assert f._tree == [4, 1, 3, 3, 10]
+
+    for idx, expected in enumerate(itertools.accumulate(array, operator.add)):
+        assert expected == f.cum_sum(idx)
+
+    assert f.ranged_cum_sum(0, 0) == 1
+    assert f.ranged_cum_sum(0, 3) == 10
+    assert f.ranged_cum_sum(1, 2) == 5
+    assert f.ranged_cum_sum(3, 3) == 4
+    f.update(3, 6)
+    assert f.read(3) == 10
+    assert f.ranged_cum_sum(0, 3) == 16
     assert f._tree[4] == 16
