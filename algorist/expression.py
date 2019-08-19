6207a483a73adbdf80b9ea1e50394e4e356c8a44
munagekar <avm.abhishek@gmail.com>
2019-08-20 01:28:10 +0900

    Add Infix to Postfix

--- algorist/expression.py
+++ algorist/expression.py
@@ -2,6 +2,8 @@ from typing import List, Union
 from collections import deque
 from operator import add, truediv, mul, sub
 
+Precendence = {"+": 0, "-": 0, "/": 1, "*": 1, "^": 2}
+
 
 def eval_postfix(expr=List[Union[float, str]]):
     stack = deque()
@@ -23,3 +25,40 @@ def eval_postfix(expr=List[Union[float, str]]):
             a = stack.pop()
             stack.append(op(a, b))
     return stack.pop()
+
+
+def infix_to_postfix(expr=List[Union[float, str]]):
+    output = []
+    stack = deque()
+    for val in expr:
+        if isinstance(val, (float, int)):
+            output.append(val)
+            continue
+        if val == "(":
+            stack.append(val)
+            continue
+        if len(stack) == 0:
+            stack.append(val)
+            continue
+        top = stack[-1]
+        if val == ")":
+            while top != "(":
+                output.append(stack.pop())
+                top = stack[-1]
+            stack.pop()
+            continue
+
+        if top == "(" or Precendence[val] > Precendence[top]:
+            stack.append(val)
+            continue
+        while top != "(" and Precendence[val] <= Precendence[top]:
+            output.append(stack.pop())
+            if len(stack) == 0:
+                break
+            top = stack[-1]
+        stack.append(val)
+
+    while len(stack) != 0:
+        output.append(stack.pop())
+
+    return output
--- tests/test_expression.py
+++ tests/test_expression.py
@@ -1,4 +1,4 @@
-from algorist.expression import eval_postfix
+from algorist.expression import eval_postfix, infix_to_postfix
 
 
 def test_eval_postfix():
@@ -6,3 +6,8 @@ def test_eval_postfix():
     assert eval_postfix(expr) == 3
     expr = [2, 3, 1, "*", "+", 9, "-"]
     assert eval_postfix(expr) == -4
+
+
+def test_infix_to_postfix():
+    a = infix_to_postfix([1, "+", 2, "*", "(", 3, "^", 4, "-", 5, ")", "^", "(", 6, "+", 7, "*", 8, ")", "-", 9])
+    assert a == [1, 2, 3, 4, "^", 5, "-", 6, 7, 8, "*", "+", "^", "*", "+", 9, "-"]
